ed9c3c67a2868669cbcf67fd0dc8738056351fa3
John Tanner <johntanner@gmail.com>
2013-08-04 16:32:56 -0700

    Create purge_script.py

---
+++ purge_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os, psutil
+
+memory_before = psutil.virtual_memory().inactive
+cpu_usage = psutil.cpu_percent(interval=1)
+
+if memory_before > 200000000:
+  if cpu_usage < 99:
+		os.system("purge")
+		memory_after = psutil.virtual_memory().inactive
+		print "Total inactive memory freed: " + str((memory_before - memory_after) / 1000000) + "MB."
+	else:
+		print "CPU usage was " + str(cpu_usage) + "%."
+else: print "Enough free memory available."
