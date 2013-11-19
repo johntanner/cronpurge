12207969e56a42a4052fee77bfde3304f8f71140
John Tanner <johntanner@gmail.com>
2013-11-18 22:36:55 -0800

    Added basic error checking.

--- purge_script.py
+++ purge_script.py
@@ -3,13 +3,23 @@
 import os, psutil
 
 memory_before = psutil.virtual_memory().inactive
-cpu_usage = psutil.cpu_percent(interval=1)
+cpu_usage = psutil.cpu_percent(interval = 1)
 
-if memory_before > 200000000:
-  if cpu_usage < 99:
+if memory_before >= 200000000:
+
+	if cpu_usage < 99:
 		os.system("purge")
 		memory_after = psutil.virtual_memory().inactive
-		print "Total inactive memory freed: " + str((memory_before - memory_after) / 1000000) + "MB."
+
+		if memory_after >= 200000000:
+			print "Unable to free memory."
+
+		elif memory_after < 200000000:
+			print "Total inactive memory freed: " + str((memory_before - memory_after) / 1000000) + "MB."
+
 	else:
 		print "CPU usage was " + str(cpu_usage) + "%."
-else: print "Enough free memory available."
+
+else: 
+	print "Enough free memory available."
+
